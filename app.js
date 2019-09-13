acadb20cb58043ab8dc4c4035ce6332975a463c5
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-13 21:14:45 +0200

    Database added

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+let Campground = mongoose.model("campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Granite Hill",
+//     image: "https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
+// }, (err, campground) => {
+//     if (err) console.log(err);
+//     else console.log(campground);
+// })
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -12,24 +34,32 @@ app.get("/", (req, res) => {
     res.render("landing");
 })
 
-let campgrounds = [{
-        name: "Salmon Creek",
-        image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
-    },
-    {
-        name: "Granite Hill",
-        image: "https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
-    },
-    {
-        name: "Mountain Goat's Rest",
-        image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
-    }
-];
+// let campgrounds = [{
+//         name: "Salmon Creek",
+//         image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
+//     },
+//     {
+//         name: "Granite Hill",
+//         image: "https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
+//     },
+//     {
+//         name: "Mountain Goat's Rest",
+//         image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
+//     }
+// ];
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", {
-        camps: campgrounds
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) console.log(err)
+        else {
+            res.render("campgrounds", {
+                camps: allCampgrounds
+            })
+        }
     })
+    // res.render("campgrounds", {
+    //     camps: campgrounds
+    // })
 })
 
 app.post("/campgrounds", (req, res) => {
@@ -39,8 +69,10 @@ app.post("/campgrounds", (req, res) => {
         name: name,
         image: image
     }
-    campgrounds.push(newCampground)
-    res.redirect("/campgrounds")
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if (err) console.log(err);
+        else res.redirect("/campgrounds")
+    })
 })
 
 app.get("/campgrounds/new", (req, res) => {
