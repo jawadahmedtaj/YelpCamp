bd6dc4d20431561e6808c5901b0325c31cf8ac0b
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-13 23:01:02 +0200

    Added another route

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp", {
 
 let campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 let Campground = mongoose.model("campground", campgroundSchema);
@@ -21,6 +22,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
+// Campground.create({
+//     name: "Granite HIll",
+//     image: "https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+//     description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
+// }, (err, campground) => {
+//     if (err) console.log(err);
+//     else console.log(campground);
+// })
+
 
 
 app.get("/", (req, res) => {
@@ -54,6 +66,15 @@ app.post("/campgrounds", (req, res) => {
 
 app.get("/campgrounds/new", (req, res) => {
     res.render("new")
+})
+
+app.get("/campgrounds/:id", (req, res) => {
+    Campground.findById(req.params.id, (err, foundCamp) => {
+        if (err) console.log(err)
+        else res.render("show", {
+            campground: foundCamp
+        })
+    })
 })
 
 app.listen(3000, () => {
