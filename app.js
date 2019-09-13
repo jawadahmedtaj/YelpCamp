26aff43dc2274f457272a9e93c841a00a64c0c3c
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-13 22:40:33 +0200

    cleanup

--- app.js
+++ app.js
@@ -16,13 +16,6 @@ let campgroundSchema = new mongoose.Schema({
 
 let Campground = mongoose.model("campground", campgroundSchema);
 
-// Campground.create({
-//     name: "Granite Hill",
-//     image: "https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
-// }, (err, campground) => {
-//     if (err) console.log(err);
-//     else console.log(campground);
-// })
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -34,19 +27,6 @@ app.get("/", (req, res) => {
     res.render("landing");
 })
 
-// let campgrounds = [{
-//         name: "Salmon Creek",
-//         image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
-//     },
-//     {
-//         name: "Granite Hill",
-//         image: "https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
-//     },
-//     {
-//         name: "Mountain Goat's Rest",
-//         image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732779d6924bcc50_340.jpg"
-//     }
-// ];
 
 app.get("/campgrounds", (req, res) => {
     Campground.find({}, (err, allCampgrounds) => {
